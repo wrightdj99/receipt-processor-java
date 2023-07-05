1c6f57b7810d0c53db0741590dccdeccf1fa22fd
Dan <danwright99@Dans-MacBook-Pro.local>
2023-07-04 23:17:15 -0500

    Starting Data Class After Holiday

---
+++ src/main/java/com/example/receiptprocessingchallengejava/Entities/Receipt.java
@@ -0,0 +1,7 @@
+package com.example.receiptprocessingchallengejava.Entities;
+
+public class Receipt {
+
+
+
+}
--- src/main/java/com/example/receiptprocessingchallengejava/Integration/ReceiptRestController.java
+++ src/main/java/com/example/receiptprocessingchallengejava/Integration/ReceiptRestController.java
@@ -1,11 +1,34 @@
 package com.example.receiptprocessingchallengejava.Integration;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.google.gson.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.*;
+
 @RestController
 public class ReceiptRestController {
+    HashMap<String, JsonObject> receiptLibrary = new HashMap<String, JsonObject>();
     @GetMapping(value = "/testing")
-    public String testCall(){
-        return "Hello! I'm the test method!";
+    public Object testCall() {
+        HashMap<String, String> hm = new HashMap<String, String>();
+        hm.put("Message", "Hello there!");
+        return hm;
+    }
+
+    @GetMapping(value = "/receipts/processes")
+    public Object createUUID(){
+        HashMap<String, String> receiptHm = new HashMap<String, String>();
+        UUID uuid = UUID.randomUUID();
+        receiptHm.put("Testing UUID", uuid.toString());
+        return receiptHm;
     }
 }
