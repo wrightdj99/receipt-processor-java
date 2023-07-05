19924e24ae3245651ae6929a501c14764eb4e0fa
Dan <danwright99@Dans-MacBook-Pro.local>
2023-07-05 16:10:21 -0500

    Starting points assigner

--- src/main/java/com/example/receiptprocessingchallengejava/Integration/ReceiptRestController.java
+++ src/main/java/com/example/receiptprocessingchallengejava/Integration/ReceiptRestController.java
@@ -2,10 +2,7 @@ package com.example.receiptprocessingchallengejava.Integration;
 import com.example.receiptprocessingchallengejava.Entities.Receipt;
 import com.google.gson.Gson;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.*;
@@ -22,7 +19,7 @@ public class ReceiptRestController {
     }
 
     @PostMapping(value = "/receipts/processes")
-    public Object createUUID(@RequestBody Receipt receipt){
+    public Object processReceipt(@RequestBody Receipt receipt){
         Gson gson = new Gson();
         HashMap<String, String> receiptHm = new HashMap<String, String>();
         String receiptString = gson.toJson(receipt);
@@ -39,4 +36,12 @@ public class ReceiptRestController {
         });
         return receiptHm;
     }
+
+    @GetMapping(value = "/receipts/{id}/points")
+    public Object getPoints(@PathVariable("id") String receiptId){
+        HashMap<String, String> returnHash = new HashMap<>();
+        returnHash.put("id", receiptId);
+        return returnHash;
+    }
+
 }
